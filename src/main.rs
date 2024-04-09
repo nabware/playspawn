a1af62326cf68e1b5e9846b5137bed1563879bbd
Nabeel Rana <nabwaregroup@gmail.com>
2024-04-08 17:06:59 -0700

    Add instance

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,83 @@
+use ash::ext::debug_utils;
+use ash::khr::{surface, wayland_surface};
+use ash::vk::{self, make_api_version};
+use std::ffi::CString;
+use std::marker::PhantomData;
+use std::ptr;
+
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
     window::WindowBuilder,
 };
 
+const APP_NAME: &'static str = "Playspawn";
+const APP_VERSION: u32 = make_api_version(0, 1, 0, 0);
+const VULKAN_VERSION: u32 = make_api_version(0, 1, 3, 0);
+
+struct VulkanApp {
+    _entry: ash::Entry,
+    instance: ash::Instance,
+}
+
+impl VulkanApp {
+    pub fn new() -> VulkanApp {
+        let app_name = CString::new(APP_NAME).unwrap();
+        let engine_name = CString::new(format!("{APP_NAME} Engine")).unwrap();
+
+        let app_info = vk::ApplicationInfo {
+            s_type: vk::StructureType::APPLICATION_INFO,
+            p_next: ptr::null(),
+            p_application_name: app_name.as_ptr(),
+            application_version: APP_VERSION,
+            p_engine_name: engine_name.as_ptr(),
+            engine_version: APP_VERSION,
+            api_version: VULKAN_VERSION,
+            _marker: PhantomData,
+        };
+
+        let extension_names = vec![
+            surface::NAME.as_ptr(),
+            wayland_surface::NAME.as_ptr(),
+            debug_utils::NAME.as_ptr(),
+        ];
+
+        let create_info = vk::InstanceCreateInfo {
+            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
+            p_next: ptr::null(),
+            flags: vk::InstanceCreateFlags::empty(),
+            p_application_info: &app_info,
+            pp_enabled_layer_names: ptr::null(),
+            enabled_layer_count: 0,
+            pp_enabled_extension_names: extension_names.as_ptr(),
+            enabled_extension_count: extension_names.len() as u32,
+            _marker: PhantomData,
+        };
+
+        let entry = unsafe { ash::Entry::load().unwrap() };
+        let instance = unsafe {
+            entry
+                .create_instance(&create_info, None)
+                .expect("Failed to create instance!")
+        };
+
+        VulkanApp {
+            _entry: entry,
+            instance,
+        }
+    }
+}
+
+impl Drop for VulkanApp {
+    fn drop(&mut self) {
+        unsafe {
+            self.instance.destroy_instance(None);
+        }
+    }
+}
+
 fn main() {
+    let _vulkan_app = VulkanApp::new();
     let event_loop = EventLoop::new().unwrap();
     let window = WindowBuilder::new().build(&event_loop).unwrap();
 
