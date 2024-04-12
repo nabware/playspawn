3b228ddd50236d6e70fbb724372f7c538a209561
Nabeel Rana <nabwaregroup@gmail.com>
2024-04-11 22:13:51 -0700

    Comment out event loop as we refactor

--- src/main.rs
+++ src/main.rs
@@ -536,40 +536,40 @@ fn main() {
     // ControlFlow::Poll.
     event_loop.set_control_flow(ControlFlow::Wait);
 
-    let _ = event_loop.run(move |event, elwt| {
-        match event {
-            Event::WindowEvent {
-                event: WindowEvent::CloseRequested,
-                ..
-            } => {
-                println!("The close button was pressed; stopping");
-                elwt.exit();
-            }
-            Event::AboutToWait => {
-                // Application update code.
+    // let _ = event_loop.run(move |event, elwt| {
+    //     match event {
+    //         Event::WindowEvent {
+    //             event: WindowEvent::CloseRequested,
+    //             ..
+    //         } => {
+    //             println!("The close button was pressed; stopping");
+    //             elwt.exit();
+    //         }
+    //         Event::AboutToWait => {
+    //             // Application update code.
 
-                // Queue a RedrawRequested event.
-                //
-                // You only need to call this if you've determined that you need
-                // to redraw in applications which do not always need to.
-                // Applications that redraw continuously can render here
-                // instead.
-                window.request_redraw();
-            }
-            Event::WindowEvent {
-                event: WindowEvent::RedrawRequested,
-                ..
-            } => {
-                // Redraw the application.
-                //
-                // It's preferable for applications that do not render
-                // continuously to render in this event rather than in
-                // AboutToWait, since rendering in here allows the program to
-                // gracefully handle redraws requested by the OS.
-            }
-            _ => (),
-        }
-    });
+    //             // Queue a RedrawRequested event.
+    //             //
+    //             // You only need to call this if you've determined that you need
+    //             // to redraw in applications which do not always need to.
+    //             // Applications that redraw continuously can render here
+    //             // instead.
+    //             window.request_redraw();
+    //         }
+    //         Event::WindowEvent {
+    //             event: WindowEvent::RedrawRequested,
+    //             ..
+    //         } => {
+    //             // Redraw the application.
+    //             //
+    //             // It's preferable for applications that do not render
+    //             // continuously to render in this event rather than in
+    //             // AboutToWait, since rendering in here allows the program to
+    //             // gracefully handle redraws requested by the OS.
+    //         }
+    //         _ => (),
+    //     }
+    // });
 }
 
 unsafe extern "system" fn debug_utils_messenger_callback(
